e4d392293ed4c544a5b4b8282eb0733f95abbde2
Farid <fareed.faisal.ghoneim@gmail.com>
2024-09-03 22:52:42 +0300

    feat: finished order service impl

--- backend/src/main/java/com/farid/backend/dto/OrderDTO.java
+++ backend/src/main/java/com/farid/backend/dto/OrderDTO.java
@@ -1,4 +1,19 @@
 package com.farid.backend.dto;
 
-public class OrderDTo {
+import com.farid.backend.entity.OrderStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Data
+@Builder
+public class OrderDTO {
+    private UUID id;
+    private double amount;
+    private LocalDateTime date;
+    private LocalDateTime shippingDate;
+    private OrderStatus status;
+    private UUID cartId;
+    private UUID paymentId;
 }
--- backend/src/main/java/com/farid/backend/service/OrderService.java
+++ backend/src/main/java/com/farid/backend/service/OrderService.java
@@ -1,16 +1,50 @@
 package com.farid.backend.service;
 
+import com.farid.backend.dto.OrderDTO;
+import com.farid.backend.entity.Order;
+import com.farid.backend.repository.CartRepository;
 import com.farid.backend.repository.OrderRepository;
+import com.farid.backend.repository.PaymentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class OrderService {
     private OrderRepository orderRepository;
+    private PaymentRepository paymentRepository;
+    private CartRepository cartRepository;
 
     public OrderDTO addOrder(OrderDTO orderDTO){
-
+        Order order = orderRepository.save(
+                Order.builder()
+                        .date(orderDTO.getDate())
+                        .shippingDate(orderDTO.getShippingDate())
+                        .amount(orderDTO.getAmount())
+                        .status(orderDTO.getStatus())
+                        .payment(paymentRepository.findById(orderDTO.getPaymentId()).get())
+                        .cart(cartRepository.findById(orderDTO.getCartId()).get())
+                        .build()
+        );
+        return orderToOrderDTO(order);
+    }
+    public OrderDTO getOrder(UUID id){
+        Optional<Order> orderOptional = orderRepository.findById(id);
+        return orderOptional.isPresent()? orderToOrderDTO(orderOptional.get()) : null;
+    }
+    public OrderDTO orderToOrderDTO(Order order){
+        return OrderDTO.builder()
+                .id(order.getId())
+                .cartId(order.getCart().getId())
+                .paymentId(order.getPayment().getId())
+                .amount(order.getAmount())
+                .status(order.getStatus())
+                .date(order.getDate())
+                .shippingDate(order.getShippingDate())
+                .build();
     }
 
 }
